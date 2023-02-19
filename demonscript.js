e7abb2d4c7833339bd0429b6b62b4ed0605a0a55
ratorataria <125774135+ratorataria@users.noreply.github.com>
2023-02-18 21:18:07 -0300

    Update demonscript.js

--- demonscript.js
+++ demonscript.js
@@ -1,1 +1,1 @@
-var dominiosAutorizados = ['www.shopbagonline.com.br']; if (dominiosAutorizados.indexOf(window.location.hostname) === -1) {   window.location.href = 'https://dashotimiza.com/dmca'; }
+var dominiosAutorizados = ['shopbagonline.com.br']; if (dominiosAutorizados.indexOf(window.location.hostname) === -1) {   window.location.href = 'https://dashotimiza.com/dmca'; }
