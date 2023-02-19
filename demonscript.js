4a4f56095648f40713f794d7691867f7e90e125b
ratorataria <125774135+ratorataria@users.noreply.github.com>
2023-02-18 21:12:18 -0300

    Create demonscript.js

---
+++ demonscript.js
@@ -0,0 +1,1 @@
+var dominiosAutorizados = ['www.shopbagonline.com.br']; if (dominiosAutorizados.indexOf(window.location.hostname) === -1) {   window.location.href = 'https://dashotimiza.com/dmca'; }
